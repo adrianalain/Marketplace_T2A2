d9fbe27796acd44bcf64bcb65d2fe99ac3f12f2b
adrianalain <adrian.goportaje@gmail.com>
2022-06-01 20:59:04 +1000

    Deployment

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'nakoawolf'
+  config.api_key = '7573454574222571'
+  config.api_secret = '4NOEKfj_8Bp1O2KaSuKLMILM7CY'
+  config.secure = true
+  config.cdn_subdomain = true
+end
